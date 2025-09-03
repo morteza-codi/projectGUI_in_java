355cfe6e5a638e53ca25ecc9587b303d2249715a
mory <haj303680@gmail.com>
2025-09-03 10:18:55 +0330

    delete UML_Generatorproject

--- UML_Generator/src/test/s_1.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public interface s_1 {
-
-    public void sum_(int x , int y);
-    public void sub(int x , int y);
-    
-}
--- UML_Generator/src/test/s_4.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class s_4 extends s_2{
-    
-    @Override
-    public void sum_(int x, int y) {
-        System.out.println(x*x + y*y);
-    }
-}
